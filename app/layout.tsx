523411fdb3821f1ed5d667c9fdab12c0b475b98a
片岡誠之郎 <kataokashigeyukirou@kataokashigeyukirounoMacBook-Air-2.local>
2025-06-09 20:28:59 +0900

    correct

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,20 @@ export default function RootLayout({
   return (
     <html lang="ja" suppressHydrationWarning>
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-P32H6K8F');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+
         {/* Google Fontsを<link>で読み込む（Noto Sans JP） */}
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap"
@@ -52,15 +66,16 @@ export default function RootLayout({
       </head>
       
       <body className="font-noto max-w-[430px] mx-auto">
-        {/* GTM noscript */}
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P32H6K8F"
             height="0"
             width="0"
             style={{ display: 'none', visibility: 'hidden' }}
           ></iframe>
         </noscript>
+        {/* End Google Tag Manager (noscript) */}
         
         <ThemeProvider
           attribute="class"
